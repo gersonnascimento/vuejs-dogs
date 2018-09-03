902362cfd097027e114aac051048c1af94f37915
Gerson Nascimento <gerson.nascimento@locaweb.com.br>
2018-09-03 18:49:53 -0300

    first

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+  <img :src="foto.url" :alt="foto.titulo">
+</div>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return{
+      titulo: 'Alurapic',
+      foto: {
+        url: 'https://i.ytimg.com/vi/8qA4UOLnC5A/maxresdefault.jpg',
+        titulo: 'cachorro'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
