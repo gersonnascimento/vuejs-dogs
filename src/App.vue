8503bde4f38e47a4b18dae353b85bf3759f55d3a
Gerson Nascimento <gerson.nascimento@locaweb.com.br>
2018-09-04 15:41:56 -0300

    created panel component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
-    <ul>
-      <li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo">
+  <div class="corpo">
+    <h1 class="titulo">{{ titulo }}</h1>
+
+
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos">
+
+        <meu-painel :titulo="foto.titulo">
+            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
+
+
       </li>
     </ul>
 </div>
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue'
+
 export default {
+
+  components:{
+    'meu-painel' : Painel
+  },
 
   data() {
     return{
@@ -27,5 +41,27 @@ export default {
 </script>
 
 <style lang="scss">
+
+  .titulo {
+    text-align: center;
+  }
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .imagem-responsiva {
+    width: 100%;
+  }
 
 </style>
