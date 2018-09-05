e61facc26c3d81e0066b358c0ea2b0b27392679e
Gerson Nascimento <gerson.nascimento@locaweb.com.br>
2018-09-05 10:04:17 -0300

    Added search method

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,13 @@
 
 
     <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="Filtre por título">
-    {{ filtro }}
+
     <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
 
         <meu-painel :titulo="foto.titulo">
             <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
         </meu-painel>
-
-
       </li>
     </ul>
 </div>
@@ -33,6 +31,19 @@ export default {
       fotos: [],
       filtro: ''
     }
+  },
+
+  computed: {
+
+    fotosComFiltro(){
+      if(this.filtro) {
+        let exp = new RegExp(this.filtro.trim(), 'i');
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      }else{
+        return this.fotos;
+      }
+    }
+
   },
   created(){
     let promise = this.$http.get('http://localhost:3000/v1/fotos');
